5c33f211f04eee068fc1b2b7459fd9297ae90ef3
Skylark <li_xuelong@126.com>
2018-01-06 20:16:50 +0800

    初始化仓库

---
+++ app/src/main/java/online/himakeit/chitchat/MainActivity.java
@@ -0,0 +1,19 @@
+package online.himakeit.chitchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @author：LiXueLong
+ * @date:2018/1/6-20:14
+ * @mail1：skylarklxlong@outlook.com
+ * @mail2：li_xuelong@126.com
+ * @des：MainActivity
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
